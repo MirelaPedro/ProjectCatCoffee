636df003395059e274be6ba27df9516a938d19f2
MirelaPedro <mirelapedrotereza@gmail.com>
2025-03-19 17:17:58 -0300

    install bibliotecas

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View, StyleSheet} from "react-native";
 import MariDoida, {Adeus} from "./components/MariDoida";
 import Home from "./Screens/home";
 import Login from "./Screens/login";
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
